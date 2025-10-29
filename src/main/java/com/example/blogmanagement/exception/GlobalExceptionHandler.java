abc9c7694a1e1a3c0462d4ff7bb4234c11e4ea64
roxane-hub <marielleroxane037@gmail.com>
2025-10-29 16:31:52 +0300

    i changed my exception handler

--- src/main/java/com/example/blogmanagement/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/blogmanagement/exception/GlobalExceptionHandler.java
@@ -2,17 +2,17 @@ package com.example.blogmanagement.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<com.example.blog.exception.ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex, WebRequest request) {
-        com.example.blog.exception.ErrorResponse error = new com.example.blog.exception.ErrorResponse(
+    public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex, WebRequest request) {
+        ErrorResponse error = new ErrorResponse(
                 HttpStatus.NOT_FOUND.value(),
                 "Not Found",
                 ex.getMessage(),
@@ -21,10 +21,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
-
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<com.example.blog.exception.ErrorResponse> handleGlobalException(Exception ex, WebRequest request) {
-        com.example.blog.exception.ErrorResponse error = new com.example.blog.exception.ErrorResponse(
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex, WebRequest request) {
+        ErrorResponse error = new ErrorResponse(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 "Internal Server Error",
                 ex.getMessage(),
